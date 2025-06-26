7a4b3c1d5a377f1606bd88e03590e38ec5026e30
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-06-26 12:31:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,23 +6,7 @@ import cvzone
 model = YOLO('yolo12s.pt')
 names = model.names
 
-# Define slanted line coordinates
-line_p1 = (344, 512)
-line_p2 = (643, 222)
 
-# To store previous center positions of each tracked ID
-hist = {}
-
-# IN/OUT counters
-in_count = 0
-out_count = 0
-
-# To avoid double-counting
-counted_ids = set()
-
-# Function to check side of point relative to a line
-def point_side_of_line(x, y, x1, y1, x2, y2):
-    return (x - x1)*(y2 - y1) - (y - y1)*(x2 - x1)
 
 # Open video file or webcam
 cap = cv2.VideoCapture("vid.mp4")  # Replace with 0 for webcam
@@ -63,35 +47,19 @@ while True:
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
             cv2.circle(frame, (cx, cy), 4, (255, 0, 0), -1)
+            cvzone.putTextRect(frame, f"ID: {track_id} {direction}", (x1, y1 + 10), scale=1, thickness=2,
+                                       colorT=(255, 255, 255), colorR=(0,0,255), offset=5, border=2)
 
-            if track_id in hist:
-                prev_cx, prev_cy = hist[track_id]
-                prev_side = point_side_of_line(prev_cx, prev_cy, *line_p1, *line_p2)
-                curr_side = point_side_of_line(cx, cy, *line_p1, *line_p2)
-                # Check if object crossed the line
-                if prev_side * curr_side < 0 and track_id not in counted_ids:
-                   if curr_side < 0:
-                      direction = "OUT"
-                      out_count += 1
-                   else:
-                      direction = "IN"
-                      in_count += 1
-                   counted_ids.add(track_id)  
-                   color = (0, 0, 255) if direction == "OUT" else (0, 255, 0)
-                   cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                   cvzone.putTextRect(frame, f"ID: {track_id} {direction}", (x1, y1 + 10), scale=1, thickness=2,
-                                       colorT=(255, 255, 255), colorR=color, offset=5, border=2)
-
-            hist[track_id] = (cx, cy)
+        
 
     # Draw counting line
-    cv2.line(frame, line_p1, line_p2, (0, 0, 255), 2)
+#    cv2.line(frame, line_p1, line_p2, (0, 0, 255), 2)
 
     # Show IN/OUT counts
-    cvzone.putTextRect(frame, f'IN: {in_count}', (316, 33), scale=2, thickness=2,
-                       colorT=(255, 255, 255), colorR=(0, 128, 0))
-    cvzone.putTextRect(frame, f'OUT: {out_count}', (316, 100), scale=2, thickness=2,
-                       colorT=(255, 255, 255), colorR=(0, 0, 255))
+ #   cvzone.putTextRect(frame, f'IN: {in_count}', (316, 33), scale=2, thickness=2,
+ #                      colorT=(255, 255, 255), colorR=(0, 128, 0))
+ #   cvzone.putTextRect(frame, f'OUT: {out_count}', (316, 100), scale=2, thickness=2,
+ #                      colorT=(255, 255, 255), colorR=(0, 0, 255))
 
     # Display frame
     cv2.imshow("RGB", frame)
